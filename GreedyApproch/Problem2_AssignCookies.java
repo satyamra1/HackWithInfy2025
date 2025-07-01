3757f689628c8b89c0fb0aab176115b79f95b0ea
satyamra1 <raisatyam710@gmail.com>
2025-07-01 11:38:29 +0530

    Updated

--- GreedyApproch/Problem2_AssignCookies.java
+++ GreedyApproch/Problem2_AssignCookies.java
@@ -1,5 +1,28 @@
 package GreedyApproch;
 
+import java.util.Arrays;
+
 public class Problem2_AssignCookies {
-    
+     public static void main(String[] args){
+        int g[]={1,2,2};
+        int s[] ={1,2,2};
+        int ans = findContentChildren(g,s);
+        System.out.println(ans);
+    }
+
+public static int findContentChildren(int[] g, int[] s) {
+        Arrays.sort(g);
+        Arrays.sort(s);
+        int n = g.length;
+        int m = s.length;
+        int i = 0;
+        int j = 0;
+
+        while (i < m && j < n) {
+            if (g[j] <= s[i]) j++;  
+            i++; 
+        }
+
+        return j;
+    }
 }
--- GreedyApproch/Problem5_Maximum_Length_of_Pair_Chain.java
+++ GreedyApproch/Problem5_Maximum_Length_of_Pair_Chain.java
@@ -1,5 +1,28 @@
 package GreedyApproch;
 
+import java.util.Arrays;
+
 public class Problem5_Maximum_Length_of_Pair_Chain {
+     public static void main(String[] args){
+        int pairs[][] = {{1,2},{2,3},{3,4}};
+      
+        int ans = findLongestChain(pairs);
+        System.out.println(ans);
+     }
     
+
+ public static int findLongestChain(int[][] pairs) {
+        Arrays.sort(pairs , (a,b)-> a[1]-b[1]);
+        int x = pairs[0][1];
+        int count = 1;
+
+        for(int i= 1 ;i<pairs.length;i++){
+            if(pairs[i][0] > x){
+                count++;
+                x=pairs[i][1];
+            }
+        }
+        return count;
+    }
+
 }
