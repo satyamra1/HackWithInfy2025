59f4206d918437cca5af0deb109b5f8b423aaa4e
satyamra1 <raisatyam710@gmail.com>
2025-07-09 11:30:45 +0530

    monu bhaiya que

---
+++ Miscellaneous/Special_city.java
@@ -0,0 +1,12 @@
+package Miscellaneous;
+import java.util.Scanner;
+
+public class Special_city {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n =sc.nextInt();
+        int x= sc.nextInt();
+        int y=sc.nextInt();
+        int[] dis = new int[n+1];
+    }
+}
